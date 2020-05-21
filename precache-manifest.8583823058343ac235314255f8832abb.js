8afa30427708e220a7a2ee699ea27b6b5a9e207b
Nelio <46218259+ioNelio@users.noreply.github.com>
2020-05-21 22:47:40 +0800

    Updates

---
+++ precache-manifest.8583823058343ac235314255f8832abb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e8d550eec49b3a6ad72cf91c93bb5544",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ad28b25207409e08514",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "e2378db2aa390495da86",
+    "url": "/static/js/2.b366afec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b366afec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ad28b25207409e08514",
+    "url": "/static/js/main.49733ecc.chunk.js"
+  },
+  {
+    "revision": "4db6ff1cb1f788afafef",
+    "url": "/static/js/runtime-main.659c8920.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
